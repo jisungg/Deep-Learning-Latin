775b718f8f4d8c913ce533ed3c401c6c1d6ddd42
jisung <jisung.lee.2005@gmail.com>
2022-12-16 14:14:14 -0800

    COMMIT - fix small issue with tokenizer

--- translatePY.py
+++ translatePY.py
@@ -141,7 +141,7 @@ eng_vocab_size = len(eng_tokenizer.word_index) + 1
 eng_length = max_length(dataset[:, 0])
 print('English Vocabulary Size: %d' % eng_vocab_size)
 print('English Max Length: %d' % (eng_length))
-# prepare german tokenizer
+# prepare latin tokenizer
 lat_tokenizer = create_tokenizer(dataset[:, 1])
 lat_vocab_size = len(lat_tokenizer.word_index) + 1
 lat_length = max_length(dataset[:, 1])
